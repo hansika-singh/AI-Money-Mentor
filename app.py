90e420364ee069935b3899399603734293d4e916
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-28 11:44:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -606,7 +606,7 @@ def loan():
 @login_required
 def get_details():
     try:
-        data = request.json() or {}
+        data = request.get_json() or {}
         principal = float(data.get("principal", 0))
         rate = float(data.get("rate", 0))
         time_years = float(data.get("time", 0))
